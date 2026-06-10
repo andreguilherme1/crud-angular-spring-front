b4fd3cbc7249db4db6ae1f26789066f24b4a1831
André Guilherme <andreguilherme645@gmail.com>
2026-06-09 21:37:52 -0300

    Criando tabela de cursos

--- src/app/components/courses/courses.ts
+++ src/app/components/courses/courses.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { CoursesService } from '../../services/courses.service';
+import { ICourses } from '../../interfaces/courses';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [MatTableModule, MatToolbarModule, MatCardModule],
   templateUrl: './courses.html',
   styleUrl: './courses.scss',
 })
-export class Courses {
+export class Courses implements OnInit {
+  displayedColumns: string[] = ['name', 'category'];
+  courses: ICourses[] = [];
 
+  private _coursesService = inject(CoursesService);
+
+  ngOnInit(): void {
+    this.getCourses();
+  }
+
+  private getCourses(): void {
+    this._coursesService.getCourses().subscribe((courses) => {
+      this.courses = courses;
+    });
+  }
 }
---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ICourses } from '../interfaces/courses';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoursesService {
+  private _http = inject(HttpClient);
+  private readonly API = '/assets/courses.json';
+
+  getCourses(): Observable<ICourses[]> {
+    return this._http.get<ICourses[]>(this.API);
+  }
+}
