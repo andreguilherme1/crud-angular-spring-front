b8e70c1f0f671aebb776aa6a81759185c43d8095
André Guilherme <andreguilherme645@gmail.com>
2026-07-07 23:36:05 -0300

    Criando formulário de cursos

--- src/app/components/courses/form/form.component.ts
+++ src/app/components/courses/form/form.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-form.component',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    RouterLink
+  ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+  form!: FormGroup;
 
+  private readonly _formBuilder = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.form = this._formBuilder.group({
+      name: [''],
+      category: [''],
+    });
+  }
+
+  onSubmit() {
+    console.log('submit')
+  }
 }
